80173338effc6e558c19950f74de7987316131cb
Nikita Laptev <nikita.laptev98@inbox.ru>
2017-10-26 22:04:46 +0300

    task 1-2

--- pokeapi.js
+++ pokeapi.js
@@ -5,4 +5,19 @@ axios.get('http://pokeapi.co/api/v2/pokemon/42')
     .then((response) => {
         console.log(`name: ${response.data.name}, weight: ${response.data.weight}, height: ${response.data.height}`);
     });
-    
+
+let requests = [];
+for (let i = 0; i < 3; i++) {
+    requests.push(axios.get(`http://pokeapi.co/api/v2/pokemon/?limit=10`));
+}
+Promise.all(requests)
+    .then((results) => {
+        results.forEach((val, resInd) => {
+            val.data.results.forEach((pokemon, ind) => {
+                console.log(`${resInd}${ind} - ${pokemon.name}`);
+            });
+        });
+    })
+    .catch((err) => {
+        console.error(err);
+    });
